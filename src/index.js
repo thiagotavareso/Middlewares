9fe713dc3ea17d9e3ade592a91067e8c26f2ecf3
Thiago Tavares <you@example.com>
2022-06-14 17:21:49 -0300

    Add function checkExistsUserAccount

--- src/index.js
+++ src/index.js
@@ -10,7 +10,18 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if(!user){
+    return response.status(400).json({erro: "Usuário não Existe"});
+  };
+
+  request.user = user;
+
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
